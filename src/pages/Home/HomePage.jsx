893ccb6253526bd7afcd7b9fd51abdc84cccf675
cesarve97 <vasquezcesza4@autonoma.edu.pe>
2025-07-09 00:04:25 -0500

    V2.2

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -8,6 +8,7 @@ const HomePage = () => {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
 
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -19,7 +20,6 @@ const HomePage = () => {
         setLoading(false)
       }
     }
-
     fetchPosts()
   }, [])
 
@@ -47,7 +47,11 @@ const HomePage = () => {
       <section id="inicio" className="hero-section">
         <div className="hero-container">
           <div className="hero-image-container">
-            <span className="hero-emoji">👋</span>
+            <img
+              src="https://www.evopayments.mx/blog/wp-content/uploads/2023/03/Como-ser-una-mujer-exitosa-en-los-negocio-770x513.jpeg"
+              alt="Foto de la autora"
+              className="hero-author-photo" // Nueva clase para la imagen
+            />            
           </div>
           <h1 className="hero-title">¡Hola, soy María!</h1>
           <p className="hero-description">Bienvenido a mi espacio personal donde comparto mis pensamientos, experiencias y pasiones sobre tecnología, viajes y vida.</p>
